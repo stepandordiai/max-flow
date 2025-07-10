e30cd24e446ad6eef7258cfc55485934a7f0b11f
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-10 10:00:09 +0200

    Update AboutUs

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -8,15 +8,15 @@ const AboutUs = () => {
 	useEffect(() => {
 		let delay = 150;
 
-		document.querySelectorAll(".about-us__counter").forEach((count) => {
+		document.querySelectorAll(".about-us__card-counter").forEach((count) => {
 			document.addEventListener("scroll", () => {
 				const countRect = count.getBoundingClientRect();
-				if (countRect.bottom < window.innerHeight) {
+				if (countRect.top < window.innerHeight) {
 					count
-						.querySelectorAll(".about-us__counter-char")
+						.querySelectorAll(".about-us__card-counter-char")
 						.forEach((char, index) => {
 							setTimeout(() => {
-								char.classList.add("about-us__counter-char--active");
+								char.classList.add("about-us__card-counter-char--active");
 							}, delay + 150 * index);
 						});
 				}
@@ -27,49 +27,51 @@ const AboutUs = () => {
 	return (
 		<div className="about-us" id="about-us">
 			<h2 className="about-us__title">{t("about_us.title")}</h2>
-			<div className="about-us__card">
-				<div>
-					<p>{t("about_us.card1_title")}</p>
-					<p className="about-us__counter">
+			<div className="about-us__cards">
+				<div className="about-us__card">
+					<p className="about-us__card-title">{t("about_us.card1_title")}</p>
+					<p className="about-us__card-counter">
 						{"50+".split("").map((char, index) => {
 							return (
-								<span key={index} className="about-us__counter-char">
+								<span key={index} className="about-us__card-counter-char">
 									{char}
 								</span>
 							);
 						})}
 					</p>
 				</div>
-				<div>
-					<p>{t("about_us.card2_title")} 12,500,000 CZK</p>
-					<p className="about-us__counter">
+				<div className="about-us__card">
+					<p className="about-us__card-title">
+						{t("about_us.card2_title")} 12,500,000 CZK
+					</p>
+					<p className="about-us__card-counter">
 						{"72".split("").map((char, index) => {
 							return (
-								<span key={index} className="about-us__counter-char">
+								<span key={index} className="about-us__card-counter-char">
 									{char}
 								</span>
 							);
 						})}
 					</p>
 				</div>
-				<div>
-					<p>{t("about_us.card3_title")}</p>
-					<p className="about-us__counter">
+				<div className="about-us__card">
+					<p className="about-us__card-title">{t("about_us.card3_title")}</p>
+					<p className="about-us__card-counter">
 						{"12".split("").map((char, index) => {
 							return (
-								<span key={index} className="about-us__counter-char">
+								<span key={index} className="about-us__card-counter-char">
 									{char}
 								</span>
 							);
 						})}
 					</p>
 				</div>
-				<div>
-					<p>{t("about_us.card4_title")}</p>
-					<p className="about-us__counter">
+				<div className="about-us__card">
+					<p className="about-us__card-title">{t("about_us.card4_title")}</p>
+					<p className="about-us__card-counter">
 						{"1000+".split("").map((char, index) => {
 							return (
-								<span key={index} className="about-us__counter-char">
+								<span key={index} className="about-us__card-counter-char">
 									{char}
 								</span>
 							);
