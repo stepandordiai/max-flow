ab5e60a50fb31e11519d1f276d75a8fcb97b0b18
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-10 13:44:30 +0200

    Update Product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,14 +1,12 @@
 import { Helmet } from "react-helmet";
-import { NavLink } from "react-router-dom";
 import "./Product.scss";
-import ContactUs from "../../components/ContactUs/ContactUs";
 import bg from "/bg3.svg";
 
 const Product = () => {
 	return (
 		<>
 			<Helmet>
-				<title>MaxFlow | Product</title>
+				<title>Product | MaxFlow</title>
 				<link rel="canonical" href="https://maxflow.cz/product" />
 			</Helmet>
 			<main className="product">
@@ -18,7 +16,7 @@ const Product = () => {
 							<div>
 								<h1>Product</h1>
 								<h2 className="product__section1-title" id="product-section-1">
-									Why MaxFlow 360&deg; for SVJ?
+									Pro SVJ
 								</h2>
 							</div>
 						</div>
@@ -38,21 +36,19 @@ const Product = () => {
 									alt=""
 								/>
 								<p style={{ marginBottom: 25, marginTop: 25 }}>
-									Finally, Fair Energy Distribution for Your Building – No More
-									Arguments or Paperwork
+									Konečně spravedlivé rozdělení energií ve vašem domě - bez
+									hádek a papírování
 								</p>
 							</div>
 							<div className="product__section1-container sect-1">
-								<p>Common Pain Points We Solve:</p>
+								<p>Problémy SVJ:</p>
 								<ul>
 									<li>
-										"Paying high energy bills with no clarity on actual usage?"
+										Platíte vysoké zálohy a nevíte, kdo energii reálně
+										spotřebovává?
 									</li>
-									<li>"Neighbors arguing over who owes what?"</li>
-									<li>
-										"Worried that solar installation will be too complicated or
-										disruptive?"
-									</li>
+									<li>Sousedi se hádají, komu účtovat přebytky?</li>
+									<li>Bojíte se, že instalace FVE bude příliš složitá?</li>
 								</ul>
 							</div>
 						</div>
@@ -65,34 +61,19 @@ const Product = () => {
 							padding: 15,
 						}}
 					>
-						<h3>Here’s How MaxFlow 360&deg; Helps:</h3>
-						<p style={{ marginBottom: 25 }}>
-							MaxFlow offers a complete energy management system built
-							specifically for SVJs with or without shared PV systems.
-						</p>
 						<div className="product__section1-container">
-							<p>Key Features for SVJs:</p>
+							<p>Řešení:</p>
 							<ul>
 								<li>
-									Automatic, Fair Allocation:
-									<span>
-										Tracks energy consumption per apartment in real time – no
-										more guesswork or disputes.
-									</span>
+									MaxFlow 360 &deg; automaticky měří a rozděluje energii na
+									základě reálné spotřeby každého bytu.
 								</li>
 								<li>
-									One-Click Billing Reports:
-									<span>
-										Our platform instantly generates monthly billing summaries
-										for each unit.
-									</span>
+									Naše cloudová platforma vám připraví vyúčtování na pár
+									kliknutí.
 								</li>
 								<li>
-									We Handle Everything:
-									<span>
-										From subsidy applications to complete installation, we take
-										care of the full process.
-									</span>
+									Postaráme se o celý proces za vás – od dotací po instalaci.
 								</li>
 							</ul>
 						</div>
@@ -117,60 +98,46 @@ const Product = () => {
 				</div>
 
 				<div>
-					<h2 id="product-section-2">MaxFlow 360&deg; Cloud Platform</h2>
+					<h2 id="product-section-2">MaxFlow 360&deg; Cloud</h2>
 					<p style={{ marginBottom: 25 }}>
-						All Your Energy Data in One Place – Anytime, Anywhere
+						Všechny údaje o energii přehledně na jednom místě – kdykoli a
+						kdekoli
 					</p>
 					<div className="product__section1-container">
-						<p>Key Features:</p>
+						<p>Vlastnosti:</p>
 						<ul>
 							<li>
-								Live Monitoring
+								Živý monitoring
+								<span>Vidíte, kolik právě teď vyrábíte a spotřebováváte.</span>
+							</li>
+							<li>
+								AI předpověď
 								<span>
-									Track real-time solar production and building consumption in
-									one simple view.
+									Systém vás upozorní, kdy prodávat přebytky za nejvýhodnější
+									cenu.
 								</span>
 							</li>
 							<li>
-								AI Energy Forecasts
-								<span>
-									Get smart alerts when it’s the best time to sell surplus
-									energy – maximize your returns automatically
-								</span>
-							</li>
-							<li>
-								Mobile App Access
-								<span>
-									Monitor, manage, and export energy data right from your phone
-									– anytime, anywhere.
-								</span>
+								Mobilní aplikace
+								<span>Správa energie z telefonu.</span>
 							</li>
 						</ul>
 					</div>
 				</div>
-				<ContactUs>
-					<p className="contact-us__title">
-						Ready to empower your SVJ with intelligent energy?
-					</p>
+				<div className="product__demo">
+					<p className="contact-us__title">Vyzkoušejte si demo dashboardu</p>
 					<div>
-						<p>
-							Let MaxFlow 360&deg; turn your building into a self-managing,
-							cost-efficient, solar-powered community.
-						</p>
-						<NavLink className="contact-us__link" to={"/contact"}>
-							<span>Chci nabídku</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								height="20px"
-								viewBox="0 -960 960 960"
-								width="20px"
-								fill="#ffffff"
-							>
-								<path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z" />
-							</svg>
-						</NavLink>
+						<form className="product__demo-form" action="">
+							<input
+								className="product__demo-input"
+								type="text"
+								placeholder="example@company.com"
+							/>
+							<button type="submit">Send</button>
+						</form>
+						<p>Zadejte e-mail a pošleme vám přístup na 24 hodin.</p>
 					</div>
-				</ContactUs>
+				</div>
 			</main>
 		</>
 	);
