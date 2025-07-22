7a357425656205efd0e1986b747779085bfb5f20
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-22 17:52:39 +0200

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,58 +2,50 @@ import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
-import styles from "./Footer.module.scss";
+import "./Footer.scss";
 
 const Footer = () => {
 	const { t } = useTranslation();
 
 	useEffect(() => {
-		const footerNavBtn = document.querySelectorAll(
-			`.${styles["footer__nav-btn"]}`
-		);
-		const footerNavBtnIcon = document.querySelectorAll(
-			`.${styles["footer__nav-btn-icon"]}`
-		);
-		const footerNavWrapper = document.querySelectorAll(
-			`.${styles["footer__nav-wrapper"]}`
-		);
+		const footerNavBtn = document.querySelectorAll(".footer__nav-btn");
+		const footerNavBtnIcon = document.querySelectorAll(".footer__nav-btn-icon");
+		const footerNavWrapper = document.querySelectorAll(".footer__nav-wrapper");
 
 		footerNavBtn.forEach((btn, index) => {
 			btn.addEventListener("click", () => {
-				footerNavWrapper[index].classList.toggle(
-					styles["footer__nav-wrapper--active"]
-				);
+				footerNavWrapper[index].classList.toggle("footer__nav-wrapper--active");
 				footerNavBtnIcon[index].classList.toggle(
-					styles["footer__nav-btn-icon--active"]
+					"footer__nav-btn-icon--active"
 				);
 			});
 
 			document.addEventListener("click", (e) => {
 				if (e.target !== btn) {
 					footerNavWrapper[index].classList.remove(
-						styles["footer__nav-wrapper--active"]
+						"footer__nav-wrapper--active"
 					);
 					footerNavBtnIcon[index].classList.remove(
-						styles["footer__nav-btn-icon--active"]
+						"footer__nav-btn-icon--active"
 					);
 				}
 			});
 		});
 	}, []);
 
-	const inactiveFooterNavLink = `${styles["footer__nav-link"]}`;
-	const activeFooterNavLink = `${styles["footer__nav-link"]} ${styles["footer__nav-link--active"]}`;
+	const inactiveFooterNavLink = "footer__nav-link";
+	const activeFooterNavLink = "footer__nav-link footer__nav-link--active";
 
 	return (
-		<footer className={styles["footer"]}>
-			<div className={styles["footer__top"]}>
-				<div className={styles["footer__nav-container"]}>
-					<button className={styles["footer__nav-btn"]}>
-						<span className={styles["footer__nav-title"]}>Navigation</span>
-						<span className={styles["footer__nav-btn-icon"]}></span>
+		<footer className="footer">
+			<div className="footer__top">
+				<div className="footer__nav-container">
+					<button className="footer__nav-btn">
+						<span className="footer__nav-title">{t("footer.navigation")}</span>
+						<span className="footer__nav-btn-icon"></span>
 					</button>
-					<div className={styles["footer__nav-wrapper"]}>
-						<nav className={styles["footer__nav"]}>
+					<div className="footer__nav-wrapper">
+						<nav className="footer__nav">
 							<NavLink
 								className={({ isActive }) =>
 									isActive ? activeFooterNavLink : inactiveFooterNavLink
@@ -105,99 +97,77 @@ const Footer = () => {
 						</nav>
 					</div>
 				</div>
-				<div className={styles["footer__nav-container"]}>
-					<button className={styles["footer__nav-btn"]}>
-						<span className={styles["footer__nav-title"]}>{t("home")}</span>
-						<span className={styles["footer__nav-btn-icon"]}></span>
+				<div className="footer__nav-container">
+					<button className="footer__nav-btn">
+						<span className="footer__nav-title">{t("home")}</span>
+						<span className="footer__nav-btn-icon"></span>
 					</button>
-					<div className={styles["footer__nav-wrapper"]}>
-						<nav className={styles["footer__nav"]}>
-							<HashLink
-								to="/#about-us"
-								smooth
-								className={styles["footer__nav-link"]}
-							>
+					<div className="footer__nav-wrapper">
+						<nav className="footer__nav">
+							<HashLink to="/#about-us" smooth className="footer__nav-link">
 								{t("about_us.title")}
 							</HashLink>
-							<HashLink
-								to="/#benefits"
-								smooth
-								className={styles["footer__nav-link"]}
-							>
+							<HashLink to="/#benefits" smooth className="footer__nav-link">
 								{t("benefits.title")}
 							</HashLink>
-							<HashLink
-								to="/#calculator"
-								smooth
-								className={styles["footer__nav-link"]}
-							>
+							<HashLink to="/#calculator" smooth className="footer__nav-link">
 								{t("calculator.title")}
 							</HashLink>
-							<HashLink
-								to={"/#partners"}
-								smooth
-								className={styles["footer__nav-link"]}
-							>
+							<HashLink to={"/#partners"} smooth className="footer__nav-link">
 								{t("partners.title")}
 							</HashLink>
 						</nav>
 					</div>
 				</div>
-				<div className={styles["footer__nav-container"]}>
-					<button className={styles["footer__nav-btn"]}>
-						<span className={styles["footer__nav-title"]}>{t("product")}</span>
-						<span className={styles["footer__nav-btn-icon"]}></span>
+				<div className="footer__nav-container">
+					<button className="footer__nav-btn">
+						<span className="footer__nav-title">{t("product")}</span>
+						<span className="footer__nav-btn-icon"></span>
 					</button>
-					<div className={styles["footer__nav-wrapper"]}>
-						<div className={styles["footer__nav"]}>
+					<div className="footer__nav-wrapper">
+						<div className="footer__nav">
 							<HashLink
-								to={"/product#product-section-1"}
+								to="/product#product-section-1"
 								smooth
-								className={styles["footer__nav-link"]}
+								className="footer__nav-link"
 							>
 								Why MaxFlow 360°
 							</HashLink>
 							<HashLink
-								to={"/product#product-section-2"}
+								to="/product#product-section-2"
 								smooth
-								className={styles["footer__nav-link"]}
+								className="footer__nav-link"
 							>
 								MaxFlow 360° Cloud Platform
 							</HashLink>
 						</div>
 					</div>
 				</div>
-				<div className={styles["footer__top-contact-us-container"]}>
+				<div className="footer__top-contact-us-container">
 					<div>
-						<p className={styles["footer__nav-title"]}>
+						<p style={{ marginBottom: 5 }} className="footer__nav-title">
 							{t("contact.call_us")}
 						</p>
-						<div className={styles["footer__nav"]}>
-							<a
-								className={styles["footer__nav-link"]}
-								href="tel:+420728803703"
-							>
+						<div className="footer__nav">
+							<a className="footer__nav-link" href="tel:+420728803703">
 								+420 728 803 703
 							</a>
 						</div>
 					</div>
 					<div>
-						<p className={styles["footer__nav-title"]}>
+						<p style={{ marginBottom: 5 }} className="footer__nav-title">
 							{t("contact.chat_to_us")}
 						</p>
-						<a
-							className={styles["footer__nav-link"]}
-							href="mailto:info@fvestavby.cz"
-						>
+						<a className="footer__nav-link" href="mailto:info@fvestavby.cz">
 							info@fvestavby.cz
 						</a>
 					</div>
 					<div>
-						<p className={styles["footer__nav-title"]}>
+						<p style={{ marginBottom: 5 }} className="footer__nav-title">
 							{t("contact.visit_us")}
 						</p>
 						<a
-							className={styles["footer__nav-link"]}
+							className="footer__nav-link"
 							href="https://maps.app.goo.gl/UBXCPbrrCNxUSXNY8"
 							target="_blank"
 						>
@@ -207,28 +177,28 @@ const Footer = () => {
 				</div>
 			</div>
 			<div>
-				<p className={styles["footer__nav-title"]}>
+				<p style={{ marginBottom: 5 }} className="footer__nav-title">
 					{t("contact.social_media")}
 				</p>
-				<div className={styles["footer__middle-socials"]}>
+				<div className="footer__middle-socials">
 					<a
-						className={styles["footer__socials-link"]}
-						href="https://www.facebook.com/people/Fve-stavby/61576683235805/"
+						className="footer__socials-link"
+						href="https://www.facebook.com/profile.php?id=61576683235805"
 						target="_blank"
 					>
 						<i className="fa-brands fa-facebook-f"></i>
 						<span>Facebook</span>
 					</a>
 					<a
-						className={styles["footer__socials-link"]}
-						href="https://www.instagram.com/fvestavby/#"
+						className="footer__socials-link"
+						href="https://www.instagram.com/fvestavby/"
 						target="_blank"
 					>
 						<i className="fa-brands fa-instagram"></i>
 						<span>Instagram</span>
 					</a>
 					<a
-						className={styles["footer__socials-link"]}
+						className="footer__socials-link"
 						href="https://www.tiktok.com/@fvestavby"
 						target="_blank"
 					>
@@ -237,15 +207,15 @@ const Footer = () => {
 					</a>
 				</div>
 			</div>
-			<div className={styles["footer__bottom"]}>
+			<div className="footer__bottom">
 				<div>
 					<p>&copy; 2025 MaxFlow</p>
-					<p>{t("all_rights_reserved")}</p>
+					<p>{t("footer.all_rights_reserved")}</p>
 				</div>
 				<p className="footer__creator">
 					Site by{" "}
 					<a
-						className={styles["footer__creator-link"]}
+						className="footer__creator-link"
 						href="https://heeeyooo.studio/"
 						target="_blank"
 					>
