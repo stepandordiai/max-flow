9ab215ad1e696483486a283211c7a37445500da4
Stepan Dordiai <stepandordiai@gmail.com>
2025-10-10 16:27:38 +0200

    Update author url in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -242,7 +242,7 @@ const Footer = () => {
 							Website created by{" "}
 							<a
 								className="footer__creator-link"
-								href="https://heeeyooo.studio/"
+								href="https://www.heeeyooo.studio/"
 								target="_blank"
 							>
 								heeeyooo studio
